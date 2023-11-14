ffa24a659f84b5a1cdeeecaa1739aa90bfefd24c
Ejimofor Ifeanyi <149477080+EjimoforIfy@users.noreply.github.com>
2023-11-14 16:38:41 -0500

    Update annualtemperatures.cpp

--- annualtemperatures.cpp
+++ annualtemperatures.cpp
@@ -6,13 +6,18 @@ int main()
 	int Newyork = 85,
 		Denver = 88,
 		Pheonix = 106;
-	PctIncrease = 0.15;
 
-	float PctIncrease = Newyork + Denver + Pheonix;
+	float PctIncrease = 0.15;
 
-	cout << "Newyork is" << Newyork << endl;
-	cout << "Denver is" << Denver << endl;
-	cout << "Pheonix is" << Pheonix << endl;
+	cout << "NY average is " << Newyork << endl;
+	cout << "Denver average is " << Denver << endl;
+	cout << "Pheonix average is " << Pheonix << endl;
+	cout << "If the averages temp rose by 15%" << endl;
 
+	cout << "NY average would be " << (Newyork * PctIncrease) + Newyork << endl;
+	cout << "Denver average would be " << (Denver * PctIncrease) + Denver << endl;
+	cout << "Pheonix average would be " << (Pheonix * PctIncrease) + Pheonix << endl;
+
+
+	
 	return 0;
-}
